0eef4976babdf4ccae21ddc4642bd40ed81f4724
Adeleye Adewale David <adeleyepamilerin9@gmail.com>
2025-12-08 12:13:40 +0100

    updated

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -63,6 +63,7 @@ const Navbar = () => {
   const navLinks = [
     { name: "Home", path: "/" },
     { name: "Courses", path: "/courses" },
+    { name: "Blog", path: "/blog" },
     { name: "About", path: "/about" },
     { name: "Contact", path: "/contact" },
   ];
